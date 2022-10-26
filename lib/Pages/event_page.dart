e5429ba324f0f1db56039008a88fe303c38e9a48
progresso85 <martin.heuripeau@gmail.com>
2022-10-26 15:22:57 +0200

    createPage event

--- lib/Pages/event_page.dart
+++ lib/Pages/event_page.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+class MyEventPage extends StatefulWidget {
+  static const String tag = "event";
+
+  @override
+  State<MyEventPage> createState() => _MyEventPageState();
+}
+
+class _MyEventPageState extends State<MyEventPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('event'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+          ],
+        ),
+      ),
+    );
+  }
+
+}
