8aca622d4bfca3748acba32c69436bc46746a254
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-10-26 11:54:39 +0200

    deleting lib/Constant.dart

--- lib/Constant.dart
+++
@@ -1,6 +0,0 @@
-const MONGO_URL =
-    "mongodb+srv://BabacHorse:G0CKoW2mdAQtvezj@cluster0.b831nmu.mongodb.net/?retryWrites=true&w=majority";
-const COLLECTION_USERS = "users";
-const COLLECTION_LESSONS = "lessons";
-const COLLECTION_PARTIES = "parties";
-const COLLECTION_HORSES = "horses";
