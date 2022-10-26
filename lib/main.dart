dda0308a4371450db506dd0cc5dbf436efbc4315
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-10-27 01:53:03 +0200

    Adding the database to main + routes of profil page

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_equitation/Pages/bonus_page.dart';
 
-import 'Pages/home_page.dart';
+// Import all pages
+import 'package:flutter_app_equitation/Pages/home_page.dart';
+import 'package:flutter_app_equitation/Pages/bonus_page.dart';
+import 'package:flutter_app_equitation/Pages/profile_page.dart';
 
 // import Mongo.dart file
 import 'Mongo.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await MongoDataBase.connect();
-  runApp(const MyApp());
+
+  var db = await MongoDataBase.connect();
+
+  runApp(MyApp(db: db));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, this.db});
 
+  final db;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      routes: {BonusPage.tag : (context)=>const BonusPage(title: 'Bonus')},
+      routes: {
+        '/': (context) => MyHomePage(title: '🏠 Home Page', db: db),
+        '/bonus': (context) => BonusPage(title:'Bonus Page'),
+        '/profil': (context) => UserProfil(db: db),
+        // '/login': (context) => LoginPage(db: db),
+        // '/register': (context) => RegisterPage(db: db),
+        // '/admin': (context) => AdminPage(db: db),
+      },
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: '🏠 Home Page'),
+
     );
   }
 }
-
-
