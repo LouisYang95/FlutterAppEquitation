7647f4de6fa5d150a30a440e08baeaaa8f97f627
N0ku <lucielecouturier2@gmail.com>
2022-10-26 11:02:23 +0200

    🔧 Add Config DataBase

---
+++ lib/Constant.dart
@@ -0,0 +1,6 @@
+const MONGO_URL =
+    "mongodb+srv://BabacHorse:G0CKoW2mdAQtvezj@cluster0.b831nmu.mongodb.net/?retryWrites=true&w=majority";
+const COLLECTION_NAME = "users";
+const COLLECTION_NAME2 = "";
+const COLLECTION_NAME3 = "";
+const COLLECTION_NAME4 = "";
