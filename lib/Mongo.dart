a546ab874b49f77080bd2369824f61c88b50fd90
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-10-27 01:53:27 +0200

    Return our db instead of collection

--- lib/Mongo.dart
+++ lib/Mongo.dart
@@ -2,15 +2,16 @@ import 'Constant.dart';
 import 'dart:developer';
 import 'package:mongo_dart/mongo_dart.dart';
 
+// Create a class to connect to the database
 class MongoDataBase {
   static connect() async {
     var db = await Db.create(MONGO_URL);
     await db.open();
     inspect(db);
     var status = db.serverStatus();
-    print(status);
-    var collection = db.collection(COLLECTION_USERS);
-    print(await collection.find().toList());
-    return collection;
+    inspect(status);
+
+    // Return db to exploit it in the main.dart file (and then in our routes)
+    return db;
   }
 }
