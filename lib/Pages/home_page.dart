20604ef74f3aab6e9933ce3f5d326c9abe25d5f7
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-10-26 11:46:39 +0200

    Creates pages folder && files + switch home_page in file

---
+++ lib/Pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'Home Page',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_equitation/bonus_page.dart';
+import 'package:flutter_app_equitation/Pages/bonus_page.dart';
+
+import 'Pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,33 +25,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Home Page',
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
