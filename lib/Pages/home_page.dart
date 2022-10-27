347f6511a660ec6fe190cfe06b67de667ed927b6
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-10-27 15:36:58 +0200

    add lessons & contests caroussel

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -18,6 +18,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  var logo = "https://cdn.discordapp.com/attachments/930039778332786718/1035170994597396530/playstore.png";
+
   final List<String> imgList = [
     'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
     'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
@@ -31,40 +33,58 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
 
     List userDate = [];
+    List contestDate = [];
+    List lessonDate = [];
+    List partyDate = [];
+
+    var dayNow = DateTime.now().millisecondsSinceEpoch;
 
     getAllUsers() async {
     // get users where creation_time exists in table users
       var users = await widget.db.collection('users').find(mongo.where.exists('creation_date')).toList();
-      //only read users contains creation_date data
-
-      var dayNow = DateTime.now().millisecondsSinceEpoch;
-
-      print("dayNow");
-      print(dayNow);
-
       //read create_date value of users
       for (var user in users) {
-
-        var name = user['username'];
-
         var dayUser = user['creation_date'];
-        print("dayUser");
-        print(dayUser);
-
         var diff = dayNow - dayUser;
-        print("diff $name");
-        print(diff);
-
         //return user with diff <= 86400000
         if (diff <= 86400000) {
-          print("user");
-          print(user);
           //add user to userDate
           userDate.add(user);
         }
       }
-      print("userDate : $userDate");
       return users;
+    }
+
+    getAllContests() async {
+      // get users where creation_time exists in table users
+      var contests = await widget.db.collection('contests').find(mongo.where.exists('creation_date')).toList();
+      //read create_date value of users
+      for (var contest in contests) {
+        var dayContest = contest['creation_date'];
+        var diff = dayNow - dayContest;
+        //return user with diff <= 86400000
+        if (diff <= 86400000) {
+          //add user to userDate
+          contestDate.add(contest);
+        }
+      }
+      return contests;
+    }
+
+    getAllLessons() async {
+      // get users where creation_time exists in table users
+      var lessons = await widget.db.collection('lessons').find(mongo.where.exists('creation_date')).toList();
+      //read create_date value of users
+      for (var lesson in lessons) {
+        var dayLesson = lesson['creation_date'];
+        var diff = dayNow - dayLesson;
+        //return user with diff <= 86400000
+        if (diff <= 86400000) {
+          //add user to userDate
+          lessonDate.add(lesson);
+        }
+      }
+      return lessons;
     }
 
     // In the slider we want into the carousel slider widget one of the images and one of the user names
@@ -83,83 +103,194 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       drawer: DrawerWidget(),
       body: Center(
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                "Nouveauté de la journée : ",
-                style: TextStyle(fontSize: 20.0, decoration: TextDecoration.underline),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child:Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  "Nouveauté de la journée : ",
+                  style: TextStyle(fontSize: 25.0, decoration: TextDecoration.underline),
+                ),
               ),
-            ),
-            FutureBuilder(
-              future: getAllUsers(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  return CarouselSlider(
-                    options: CarouselOptions(
-                      height: 390.0,
-                      autoPlay: false,
-                      autoPlayInterval: const Duration(seconds: 3),
-                      autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                      autoPlayCurve: Curves.fastOutSlowIn,
-                      enlargeCenterPage: true,
-                      scrollDirection: Axis.horizontal,
-                    ),
-                    items: userDate.map<Widget>((user) {
+              const Padding(
+                padding: EdgeInsets.all(1.0),
+                child: Text(
+                  "Cavaliers : ",
+                  style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),
+                ),
+              ),
+              FutureBuilder(
+                future: getAllUsers(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return CarouselSlider(
+                      options: CarouselOptions(
+                        height: 310.0,
+                        autoPlay: (userDate.length < 2 ? false : true),
+                        autoPlayInterval: const Duration(seconds: 3),
+                        autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                        autoPlayCurve: Curves.fastOutSlowIn,
+                        enlargeCenterPage: true,
+                        scrollDirection: Axis.horizontal,
+                        viewportFraction: 0.6,
+                      ),
+                      items: userDate.map<Widget>((user) {
 
-                      if (user['creation_date'] != null) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                              width: 300,
+                              height: 200,
+                              margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 30.0),
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: SingleChildScrollView(
+                                scrollDirection: Axis.vertical,
+                                child: Column(
+                                  children: [
+                                    Image.network(imgList[0]),
+                                    const SizedBox(height: 10.0),
+                                    const Text("Nouveau Cavalier !", style: const TextStyle(fontSize: 20.0, color: Colors.black, fontWeight: FontWeight.bold)),
+                                    const SizedBox(height: 10.0),
+                                    Text(user['username'], style: const TextStyle(fontSize: 16.0, color: Colors.black, decoration: TextDecoration.underline)),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
 
-                        // var dayNow = DateTime.now().millisecondsSinceEpoch;
-                        // var dayUser = snapshot.data[6]['creation_date'];
-                        //
-                        // print("Date Now : ${DateTime.now().millisecondsSinceEpoch}");
-                        // print("Date User : ${snapshot.data[6]['creation_date']}");
-                        //
-                        // print(dayNow - dayUser);
-                        //
-                        // if (dayNow - dayUser <= 86400) {
-                          return Builder(
-                            builder: (BuildContext context) {
-                              return Container(
-                                width: MediaQuery.of(context).size.width,
-                                height: MediaQuery.of(context).size.height,
-                                margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 30.0),
-                                decoration: const BoxDecoration(
-                                  color: Colors.white,
+                      }).toList(),
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+              const Padding(
+                padding: EdgeInsets.all(1.0),
+                child: Text(
+                  "Contests : ",
+                  style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),
+                ),
+              ),
+              FutureBuilder(
+                future: getAllContests(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return CarouselSlider(
+                      options: CarouselOptions(
+                        height: 310.0,
+                        autoPlay: (contestDate.length < 2 ? false : true),
+                        autoPlayInterval: const Duration(seconds: 3),
+                        autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                        autoPlayCurve: Curves.fastOutSlowIn,
+                        enlargeCenterPage: true,
+                        scrollDirection: Axis.horizontal,
+                        viewportFraction: 0.6,
+                      ),
+                      items: contestDate.map<Widget>((user) {
+
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                              width: 300,
+                              height: 200,
+                              margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 30.0),
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: SingleChildScrollView(
+                                scrollDirection: Axis.vertical,
+                                child: Column(
+                                  children: [
+                                    Image.network(imgList[1]),
+                                    const SizedBox(height: 10.0),
+                                    const Text("New Contest !", style: const TextStyle(fontSize: 20.0, color: Colors.black, fontWeight: FontWeight.bold)),
+                                    const SizedBox(height: 10.0),
+                                    Text(user['name'], style: const TextStyle(fontSize: 16.0, color: Colors.black, decoration: TextDecoration.underline)),
+                                  ],
                                 ),
-                                child: SingleChildScrollView(
-                                  scrollDirection: Axis.vertical,
-                                  child: Column(
-                                    children: [
-                                      Image.network(imgList[0]),
-                                      Text(user['username'], style: const TextStyle(fontSize: 16.0, color: Colors.black)),
-                                      Text(user['email'], style: const TextStyle(fontSize: 16.0, color: Colors.black)),
-                                      Text(user['password'], style: const TextStyle(fontSize: 16.0, color: Colors.black)),
-                                      Text(user['creation_date'].toString(), style: const TextStyle(fontSize: 16.0, color: Colors.black)),
-                                    ],
-                                  ),
+                              ),
+                            );
+                          },
+                        );
+
+                      }).toList(),
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+              const Padding(
+                padding: EdgeInsets.all(1.0),
+                child: Text(
+                  "Lessons : ",
+                  style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),
+                ),
+              ),
+              FutureBuilder(
+                future: getAllLessons(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return CarouselSlider(
+                      options: CarouselOptions(
+                        height: 310.0,
+                        autoPlay: (lessonDate.length < 2 ? false : true),
+                        autoPlayInterval: const Duration(seconds: 3),
+                        autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                        autoPlayCurve: Curves.fastOutSlowIn,
+                        enlargeCenterPage: true,
+                        scrollDirection: Axis.horizontal,
+                        viewportFraction: 0.6,
+                      ),
+                      items: lessonDate.map<Widget>((user) {
+
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                              width: 300,
+                              height: 200,
+                              margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 30.0),
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: SingleChildScrollView(
+                                scrollDirection: Axis.vertical,
+                                child: Column(
+                                  children: [
+                                    Image.network(imgList[2]),
+                                    const SizedBox(height: 10.0),
+                                    const Text("New Lesson !", style: const TextStyle(fontSize: 20.0, color: Colors.black, fontWeight: FontWeight.bold)),
+                                    const SizedBox(height: 10.0),
+                                    Text(user['land'], style: const TextStyle(fontSize: 16.0, color: Colors.black, decoration: TextDecoration.underline)),
+                                  ],
                                 ),
-                              );
-                            },
-                          );
-                        // } else {
-                        //   return Image.network(imgList[0]);
-                        // }
-                      }else {
-                        return const Text("");
-                      }
-                    }).toList(),
-                  );
-                } else {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
-            ),
-          ],
+                              ),
+                            );
+                          },
+                        );
+
+                      }).toList(),
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+            ],
         ),
+      ),
       ),
     );
   }
