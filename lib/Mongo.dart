42c4decf08917ef7a98f31195fe51bdcacb2f8c1
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-10-27 10:34:49 +0200

    Comment useless var / print mongo.dart

--- lib/Mongo.dart
+++ lib/Mongo.dart
@@ -8,9 +8,28 @@ class MongoDataBase {
     await db.open();
     inspect(db);
     var status = db.serverStatus();
-    print(status);
-    var collection = db.collection(COLLECTION_USERS);
-    print(await collection.find().toList());
-    return collection;
+    // print(status);
+    // var collection = db.collection(COLLECTION_USERS);
+    // print(await collection.find().toList());
+    //
+    // await collection.insertMany([
+    //   {
+    //     "username": 'test 1',
+    //     "password": 'test1',
+    //     "email": 'test1@gmail.com',
+    //   },
+    //   {
+    //     "username": 'test 2',
+    //     "password": 'test2',
+    //     "email": 'test2@gmail.com',
+    //   },
+    //   {
+    //     "username": 'test 3',
+    //     "password": 'test3',
+    //     "email": 'test3@gmail.com',
+    //   }
+    // ]);
+
+    return db;
   }
 }
