1b59b4d1e260342eefd9d1ebbf0e6f4d6faeb341
progresso85 <martin.heuripeau@gmail.com>
2022-10-27 09:42:07 +0200

    create dropdownButtonFormField
    need dropdownValue

--- lib/Pages/event_page.dart
+++ lib/Pages/event_page.dart
@@ -20,7 +20,34 @@ class _MyEventPageState extends State<MyEventPage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButtonFormField<String>(
+              dropdownColor: const Color.fromARGB(245, 215, 194, 239),
+              value: dropdownValue,
+              icon: const Icon(Icons.arrow_downward),
+              elevation: 16,
+              style: const TextStyle(color: Colors.deepPurple),
+              validator: (String? value) {
+                if (value == 'Type of the Class') {
+                  return "Type required";
+                }
+                return null;
+              },
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue!;
+                });
+              },
+              items: const <DropdownMenuItem<String>>[
+                DropdownMenuItem(
+                    value: 'Type of the Class',
+                    child: Text('Type of the Class')),
+                DropdownMenuItem(value: 'Aperitif', child: Text('Aperitif')),
+                DropdownMenuItem(
+                    value: 'Meals', child: Text('Meals')),
+              ],
+            ),
+          ],
         ),
       ),
     );
